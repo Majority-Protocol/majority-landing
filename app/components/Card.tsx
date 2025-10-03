5eed134d324a9d0095171c37fd974f9b557273a3
Mark Vujevits <vujevits.mark@gmail.com>
2025-10-03 10:59:26 +0200

    update images and fix height issue

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,8 +1,8 @@
 export const Card = ({ title, description, image }: { title: string, description: string, image: string }) => {
   return (
-    <div className="bg-amber-400/20 rounded-xl w-full min-w-[300px] max-w-[411px] max-h-[424px] flex flex-col">
+    <div className="bg-amber-400/20 rounded-xl w-full min-w-[300px] max-w-[411px] max-h-[424px] min-h-[360px] overflow-hidden flex flex-col">
       <div className="p-4 flex flex-col flex-1">
-        <div className="flex-1 mb-4 overflow-hidden rounded-lg">
+        <div className="h-[200px] mb-4 overflow-hidden rounded-lg">
           <img src={image} alt={title} className="w-full h-full object-cover" />
         </div>
         <h3 className="text-xl font-bold mb-2">{title}</h3>
